5606767b4c415ffe27ece418bf3ab153bb6b07b8
Machel Nyanumba <machelnyanumba14@gmail.comgit config --global user.email machelnyanumba14@gmail.com>
2019-03-31 18:18:06 +0300

    renamed the name in input tag and returned the button

--- js/scripts.js
+++ js/scripts.js
@@ -1,29 +1,34 @@
+function add(number1, number2, number3, number4, number5){
+  return number1 + number2 + number3 + number4 + number5;
+};
+
+
 $(document).ready(function() {
-  $("#button").click(function() {
+  $("#quiz").submit(function(event){
+    event.preventDefault();
+var numb1 = $("input:radio[name=number1]:checked").val();
+var numb2 = $("input:radio[name=number2]:checked").val();
+var numb3 = $("input:radio[name=number3]:checked").val();
+var numb4 = $("input:radio[name=number4]:checked").val();
+var numb5 = $("input:radio[name=number5]:checked").val();
 
-var numb1 = $("input[name=number1]:checked").val();
-var numb2 = $("input[name=number2]:checked").val();
-var numb3 = $("input[name=number3]:checked").val();
-var numb4 = $("input[name=number4]:checked").val();
-var numb5 = $("input[name=number5]:checked").val();
+var val1 = parseInt(numb1);
+var val2 = parseInt(numb2);
+var val3 = parseInt(numb3);
+var val4 = parseInt(numb4);
+var val5 = parseInt(numb5);
+var result = (val1 + val2 + val3 + val4 + val5);
 
-var answers = 0
+$(".dip").text(" Your score is: " + result " points out of 50");
 
-if(numb1=="Netscape"){
-  answers++;
+if(result >= 40){
+   $("#comment").text("excellent work.")
+} else if (result >= 25 && result < 40) {
+  $("#comment").text("fairly done")
+}else if (result < 25) {
+  $("#comment").text("poor work!! retake the test")
+} else }
+  $("#comment").text("answer every question please!!!")
 }
-  if (numb2=="//") {
-  answers++;
-}
-if (numb3=="prompt") {
-  answers++;
-}
-if (numb4=="script") {
-  answers++;
-}
-if (numb5=="a programming language") {
-  answers++;
-}
-document.getElementById('Qans').innerHTML="the result you got is " + answers + " out of the 5 questions you did"
-})
-})
+});
+});
