1ba211da8b4dc20b245eced59b3cc79f840826d3
Machel Nyanumba <machelnyanumba14@gmail.comgit config --global user.email machelnyanumba14@gmail.com>
2019-04-01 09:47:21 +0300

    made alot of changes in html and scripts

--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,30 @@
 //backend logic
-function add(number1, number2, number3, number4, number5){
-  return number1 + number2 + number3 + number4 + number5;
-};
-
 $(document).ready(function() {
-  $(".quiz").submit(function(event){
-    event.preventDefault();
+  $(".quiz").click(function(event){
+  
 var number1 = parseInt($("input:radio[name=ans1]:checked").val());
 var number2 = parseInt($("input:radio[name=ans2]:checked").val());
 var number3 = parseInt($("input:radio[name=ans3]:checked").val());
 var number4 = parseInt($("input:radio[name=ans4]:checked").val());
 var number5 = parseInt($("input:radio[name=ans5]:checked").val());
-var result = add()
-$(".dip").text(" Your score is: " + result " points out of 50");
+function add(number1, number2, number3, number4, number5){
+  return number1 + number2 + number3 + number4 + number5;
+};
+var result = add(number1, number2, number3, number4, number5);
+
+$(".dip").text(result);
+
+
 
 //frontend logic
 if(result >= 40){
-   $("#comment").text("excellent work.")
+   $("#output").text("excellent work.")
 } else if (result >= 25 && result < 40) {
-  $("#comment").text("fairly done.")
+  $("#output").text("fairly done.")
 }else if (result < 25) {
-  $("#comment").text("poor work!! retake the test.")
-} else }
-  $("#comment").text("answer every question please!!!")
+  $("#output").text("poor work!! retake the test.")
+} else {
+  $("#output").text("answer every question please!!!")
 }
 });
 });
